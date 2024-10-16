2b6cfb7096bfc66fa55401ed903bd9877ee4f49b
Jackson Roberio <jackson.roberio.dev@gmail.com>
2024-10-16 19:14:28 -0300

    :bricks: feat: inserido código fonte desafio 03

---
+++ midia/desafio/03/codigo_fonte.ino
@@ -0,0 +1,18 @@
+#define sensor 27
+#define led 13
+
+void setup() {
+  pinMode(sensor, INPUT);
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+
+  if(digitalRead(sensor)){
+    digitalWrite(led, HIGH);
+    delay(100);
+    digitalWrite(led, LOW);
+    delay(100);
+  }
+  
+}
