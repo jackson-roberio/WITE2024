9dca7ac0e999df901564e77c8363966ee67f7930
Jackson Roberio <jackson.roberio.dev@gmail.com>
2025-11-16 17:51:16 -0300

    Update codigo_fonte.ino.ino

--- midia/desafio/pega-ladrao/codigo_fonte.ino.ino
+++ midia/desafio/pega-ladrao/codigo_fonte.ino.ino
@@ -2,29 +2,34 @@
 * WITE 2024
 * Robótica Educacional
 * Desafio : Pega Ladrão
-* Microcontrolador	: ESP8266
-* Autor				: Jackson Roberio - https://jacksonroberio.com.br
-**/ 
+* Microcontrolador: Arduino
+* Autor: Jarles Santos
+*/ 
 
-#define AutoFalante 8
-#define presenca 4
+
+
+#define buzzer 7                                  //define a porta digital 7 do arduino para o buzzer.
+#define sensorPresenca 5                          //define a porta digital 5 do arduino para o sensor de presença.
 
 void setup() {
-  pinMode(AutoFalante, OUTPUT);
-  pinMode(presenca, INPUT);
+  pinMode(buzzer, OUTPUT);                        //define a porta digital 5, do buzzer, como uma saída de dados (som do alarme).
+  pinMode(sensorPresenca, INPUT);                 //define a porta digital 7, do sensor, como uma entrada de dados (leitura de movimento).
+  Serial.begin(9600);                             //ativa o monitor serial, para exibir o texto "MOVIMENTO DETECTADO" ou "SEM MOVIMENTO".
 }
 
 void loop() {
-  bool valorSensor = digitalRead(presenca);
+  bool valorSensor = digitalRead(sensorPresenca); // cria uma variável blooleada (pode ser apenas verdadeira ou falsa) e armazena o status do sensor de presença, se detectou movimento ou não.
   
-  if (valorSensor){
-    Serial.println("DETECTADO");
-    for (int i = 0; i <= 5; i++){
-      tone(AutoFalante, 300);
-      delay(500);
-      noTone(AutoFalante);
-      delay(500);
-    }
-  } 
+  if (valorSensor){                               // verifica se o sensor detectou movimento.
+      Serial.println("MOVIMENTO DETECTADO!");     // confirmado o movimento detectado, exibe a mensagem "MOVIMENTO DETECTADO".
+      digitalWrite(buzzer, HIGH);                 // liga a saída digital relativa ao buzzer, acionando o alarme.
+      
+  }
+    else {                                        // caso não haja movimento detectado pelo sensor, entrará nesta condição.
+        digitalWrite(buzzer, LOW);                // desliga a saída digital relativa ao buzzer, mantendo o alarme desligado.
+        Serial.println("SEM MOVIMENTO!");         // confirmado que não há movimento detectado, exibe a mensagem "SEM MOVIMENTO".
+     
+      
+  }
 
 }
